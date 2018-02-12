85ad5451121c97b9f499e3971be190699aa0e5ab
ravisankar426 <ravisankar426@gmail.com>
2018-02-11 22:18:55 -0500

    Added the collide method.

--- LinkedLists/Program.cs
+++ LinkedLists/Program.cs
@@ -538,6 +538,32 @@ namespace LinkedLists
 
             return head;
         }
+
+        public Node Collide(Node head)
+        {
+            Node slow = head;
+            Node fast = head;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (fast == slow)
+                    break;
+            }
+
+            if (fast == null || fast.next == null)
+                return null;
+
+            slow = head;
+            while (slow != fast)
+            {
+                slow = slow.next;
+                fast = fast.next;
+            }
+
+            return fast;
+        }
     }
 
 
